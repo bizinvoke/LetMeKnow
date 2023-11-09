49d5fb4a4428432f1827cfa0f5cb5b27ead8b04e
nextdooroldwang <278188438@qq.com>
2023-11-09 22:06:36 +0900

    api

---
+++ src/components/common/upload/index.tsx
@@ -0,0 +1,177 @@
+// import type { ResponseModel } from '@/types/shared';
+// import fetcher from '@/util/request';
+import dayjs from 'dayjs';
+import { useEffect, useRef } from 'react';
+
+export type UploadFilePathModel = {
+  createDate: dayjs.Dayjs;
+  path: string;
+  url: string;
+};
+
+type Props = {
+  onChange: (files: string[]) => void;
+  // onSuccess: (files: UploadFilePathModel[]) => void;
+  onFinally?: () => void;
+  isCompress?: boolean;
+  isOpen?: boolean;
+  multiple?: boolean;
+};
+
+export default function ImageUploadInput({
+  isOpen,
+  multiple,
+  onChange,
+  isCompress,
+  // onSuccess,
+  onFinally,
+}: Props) {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const handleUpdateImage = () => {
+    if (fileInputRef) fileInputRef.current?.click();
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      handleUpdateImage();
+      if (onFinally !== undefined) {
+        onFinally();
+      }
+    }
+  }, [isOpen, onFinally]);
+
+  // const requestUpload = (file: File, createDate: dayjs.Dayjs) => {
+  //   const formData = new FormData();
+  //   formData.append('file', file);
+  //   fetcher({
+  //     uri: `/common/file/upload?biz_code=${type}`,
+  //     headers: {
+  //       Accept: '*/*',
+  //     },
+  //     method: 'PUT',
+  //     parameters: formData,
+  //   }).then((res: ResponseModel<PureValue>) => {
+  //     if (res.code === 1000) {
+  //       onSuccess([
+  //         {
+  //           createDate,
+  //           path: res.data.filePath,
+  //           url: res.data.fileUrl,
+  //         },
+  //       ]);
+  //     }
+  //   });
+  // };
+
+  function compressImage(file: File): Promise<File> {
+    return new Promise((resolve, reject) => {
+      const maxSize = 1024; // 你想要的最大文件大小，单位为KB
+      const reader = new FileReader();
+
+      reader.onload = (event) => {
+        const img = new Image();
+        img.src = event.target?.result as string;
+
+        img.onload = () => {
+          const canvas = document.createElement('canvas');
+          const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+          const maxWidth = 800; // 你想要的最大宽度
+          const maxHeight = 600; // 你想要的最大高度
+
+          let width = img.width;
+          let height = img.height;
+
+          if (width > height) {
+            if (width > maxWidth) {
+              height *= maxWidth / width;
+              width = maxWidth;
+            }
+          } else {
+            if (height > maxHeight) {
+              width *= maxHeight / height;
+              height = maxHeight;
+            }
+          }
+
+          canvas.width = width;
+          canvas.height = height;
+
+          ctx.drawImage(img, 0, 0, width, height);
+
+          canvas.toBlob(
+            (blob) => {
+              const compressedFile = new File([blob as Blob], file.name, {
+                type: file.type,
+              });
+              resolve(compressedFile);
+            },
+            file.type,
+            0.7,
+          ); // 0.7 是压缩质量，可以调整
+        };
+
+        img.onerror = reject;
+      };
+
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    });
+  }
+
+  function readFileAsBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        const base64String = reader.result as string;
+        resolve(base64String);
+      };
+
+      reader.onerror = reject;
+
+      if (isCompress === true) {
+        compressImage(file)
+          .then((compressedFile) => reader.readAsDataURL(compressedFile))
+          .catch(reject);
+      } else reader.readAsDataURL(file);
+    });
+  }
+
+  async function convertFilesToBase64(files: File[]): Promise<string[]> {
+    const base64Strings: string[] = [];
+
+    for (const file of files) {
+      const base64String = await readFileAsBase64(file);
+      base64Strings.push(base64String);
+    }
+
+    return base64Strings;
+  }
+
+  const handleSendMediaMessage = async (files: FileList) => {
+    const fileArray = Array.from(files);
+
+    onChange(await convertFilesToBase64(fileArray));
+  };
+
+  return (
+    <>
+      <input
+        ref={fileInputRef}
+        type="file"
+        accept="image/*"
+        multiple={multiple ?? true}
+        hidden
+        onChange={() => {
+          if (
+            fileInputRef.current?.files &&
+            fileInputRef.current?.files.length > 0
+          ) {
+            handleSendMediaMessage(fileInputRef.current?.files);
+          }
+        }}
+      />
+    </>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,37 @@
+import ImageUploadInput from '@/components/common/upload';
 import {
   CameraFilled,
   MenuOutlined,
   RadiusSettingOutlined,
 } from '@ant-design/icons';
 import { Button, Divider, Drawer, Form, Image, Select } from 'antd';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 const { Option } = Select;
 function Home() {
   const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isRotate, setIsRotate] = useState(false);
   const [language, setLanguage] = useState(1);
   const [type, setType] = useState(1);
+  const [content, setContent] = useState('');
+  const [images, setImages] = useState<string[]>([]);
 
   const handleSubmit = () => {
+    const messages = [];
+    images.forEach((i) => {
+      messages.push({
+        role: 'user',
+        content: [
+          { type: 'text', text: 'What’s in this image?' },
+          {
+            type: 'image_url',
+            image_url: {
+              url: i,
+            },
+          },
+        ],
+      });
+    });
     fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: {
@@ -19,20 +39,20 @@ function Home() {
         Authorization: `Bearer ${import.meta.env.VITE_OPEN_AI_KEY}`,
       },
       body: JSON.stringify({
-        model: 'gpt-3.5-turbo-1106',
-        messages: [
-          {
-            role: 'user',
-            content: ['批改作业'],
-          },
-        ],
+        model: 'gpt-4-vision-preview',
+        messages,
         temperature: 0,
         max_tokens: 256,
       }),
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.choices[0].text);
+        setIsRotate((old) => !old);
+        console.log(data);
+
+        if (data?.choices[0]?.message?.content) {
+          setContent(data?.choices[0]?.message?.content);
+        }
       })
       .catch((error) => {
         console.error(error);
@@ -65,9 +85,9 @@ function Home() {
     </Drawer>
   );
 
-  const imageBox = (
+  const imageBox = (src: string) => (
     <div className="h-24 w-24 rounded-md border p-2">
-      <Image src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
+      <Image src={src} />
     </div>
   );
 
@@ -82,29 +102,43 @@ function Home() {
         <RadiusSettingOutlined />
       </div>
       <div className="w-full flex-1 overflow-hidden p-2">
-        <div className="h-full w-full rounded bg-white shadow-md">
-          <div className="flex h-full w-full flex-col items-center">
-            {/* <div className="h-8 w-full bg-indigo-100">
+        <div
+          className="h-full w-full rounded bg-white shadow-md"
+          style={{
+            transition: 'transform 1s',
+            transform: isRotate ? 'rotateY(180deg)' : undefined,
+          }}
+        >
+          {isRotate ? (
+            <div
+              className="h-full w-full rounded border bg-slate-100 p-2"
+              style={{ transform: 'rotateY(180deg)' }}
+              dangerouslySetInnerHTML={{ __html: content }}
+            ></div>
+          ) : (
+            <div className="flex h-full w-full flex-col items-center">
+              {/* <div className="h-8 w-full bg-indigo-100">
 
             </div> */}
-            <CameraFilled
-              style={{
-                fontSize: '16rem',
-                color: 'var(--primary-color)',
-                marginTop: '4rem',
-              }}
-            />
-            <div
-              className="flex flex-wrap gap-2 p-2"
-              style={{ minHeight: '6rem', marginTop: '6rem' }}
-            >
-              <Divider />
-              {imageBox}
-              {imageBox}
-              {imageBox}
-              {imageBox}
+              <CameraFilled
+                style={{
+                  fontSize: '16rem',
+                  color: 'var(--primary-color)',
+                  marginTop: '4rem',
+                }}
+                onClick={() => setIsOpen(true)}
+              />
+              <div
+                className="flex flex-wrap gap-2 p-2"
+                style={{ minHeight: '6rem', marginTop: '6rem' }}
+              >
+                <Divider />
+                {images.map((i) => (
+                  <Fragment key={i.slice(10)}>{imageBox(i)}</Fragment>
+                ))}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
       <div className="w-full px-2 pb-2">
@@ -113,6 +147,12 @@ function Home() {
         </Button>
       </div>
       {optionDrawer}
+      <ImageUploadInput
+        isOpen={isOpen}
+        onChange={setImages}
+        onFinally={() => setIsOpen(false)}
+        isCompress
+      />
     </div>
   );
 }
