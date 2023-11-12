b9d6cbfc437f0413dd587e6d2974c92ed1f58724
nextdooroldwang <278188438@qq.com>
2023-11-12 13:44:35 +0900

    more

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,48 @@
 import ImageUploadInput from '@/components/common/upload';
 import {
+  languageMap,
+  languageOptions,
+  subjectMap,
+  subjectOptions,
+} from '@/types/home/map';
+import {
   CameraFilled,
   MenuOutlined,
+  PlusOutlined,
   RadiusSettingOutlined,
 } from '@ant-design/icons';
-import { Button, Divider, Drawer, Form, Image, Input, Select } from 'antd';
+import {
+  Button,
+  Checkbox,
+  Divider,
+  Drawer,
+  Form,
+  Image,
+  Input,
+  Select,
+  Switch,
+  Typography,
+} from 'antd';
 import { Fragment, useState } from 'react';
 const { Option } = Select;
+const { Text } = Typography;
 function Home() {
   const [open, setOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpenTitle, setIsOpenTitle] = useState(false);
+  const [isOpenQuestion, setIsOpenQuestion] = useState(false);
+  const [hasAttached, setHasAttached] = useState(false);
+  const [isOpenAttached, setIsOpenAttached] = useState(false);
   const [isRotate, setIsRotate] = useState(false);
   const [language, setLanguage] = useState(1);
   const [type, setType] = useState(1);
+  const [subject, setSubject] = useState(1);
   const [content, setContent] = useState('');
   const [key, setKey] = useState('');
   const [images, setImages] = useState<string[]>([]);
+  const [titleImages, setTitleImages] = useState<string[]>([]);
+  const [questionImages, setQuestionImages] = useState<string[]>([]);
+  const [attachedImages, setAttachedImages] = useState<string[]>([]);
 
   const handleSubmit = () => {
     const messages: any[] = [];
@@ -75,18 +102,23 @@ function Home() {
         <Input value={key} onChange={(e) => setKey(e.target.value)} />
       </Form.Item>
       <Divider />
-      <Form.Item name="type" label="模式">
-        <Select onChange={setType} value={type} defaultValue={1}>
-          <Option value={1}>简单</Option>
-          <Option value={2}>复杂</Option>
+      <Form.Item name="subject" label="科目">
+        <Select onChange={setSubject} value={subject} defaultValue={1}>
+          {subjectOptions.map((i) => (
+            <Option value={i.value} key={i.value}>
+              {i.label}
+            </Option>
+          ))}
         </Select>
       </Form.Item>
       <Divider />
-      <Form.Item name="language" label="语言">
+      <Form.Item name="language" label="输出">
         <Select onChange={setLanguage} value={language} defaultValue={1}>
-          <Option value={1}>日语</Option>
-          <Option value={2}>英语</Option>
-          <Option value={3}>汉语</Option>
+          {languageOptions.map((i) => (
+            <Option value={i.value} key={i.value}>
+              {i.label}
+            </Option>
+          ))}
         </Select>
       </Form.Item>
       <Divider />
@@ -95,7 +127,21 @@ function Home() {
 
   const imageBox = (src: string) => (
     <div className="h-24 w-24 rounded-md border p-2">
-      <Image src={src} />
+      <Image
+        src={src}
+        width="100%"
+        height="100%"
+        style={{ objectFit: 'cover' }}
+      />
+    </div>
+  );
+
+  const uploadBox = (handleClick: () => void) => (
+    <div
+      className="anticon flex h-24 w-24 items-center justify-center rounded-md border-2 border-dashed p-2"
+      onClick={handleClick}
+    >
+      <PlusOutlined />
     </div>
   );
 
@@ -125,26 +171,99 @@ function Home() {
             ></div>
           ) : (
             <div className="flex h-full w-full flex-col items-center">
-              {/* <div className="h-8 w-full bg-indigo-100">
-
-            </div> */}
-              <CameraFilled
-                style={{
-                  fontSize: '16rem',
-                  color: 'var(--primary-color)',
-                  marginTop: '4rem',
-                }}
-                onClick={() => setIsOpen(true)}
-              />
               <div
-                className="flex flex-wrap gap-2 p-2"
-                style={{ minHeight: '6rem', marginTop: '6rem' }}
+                className={`flex h-8 w-full items-center justify-between rounded ${
+                  type === 2 ? 'bg-indigo-100' : 'bg-gray-100'
+                } px-4`}
+                style={{
+                  borderBottomLeftRadius: 0,
+                  borderBottomRightRadius: 0,
+                }}
               >
-                <Divider />
-                {images.map((i) => (
-                  <Fragment key={i.slice(10)}>{imageBox(i)}</Fragment>
-                ))}
+                <Text type="secondary" strong>
+                  科目:{subjectMap.get(subject)} | 输出:
+                  {languageMap.get(language)}
+                </Text>
+                <div className="flex items-center">
+                  <Text type="secondary" strong>
+                    精准模式：
+                  </Text>
+                  <Switch
+                    checked={type === 2}
+                    onChange={(isCheck) => setType(isCheck ? 2 : 1)}
+                  ></Switch>
+                </div>
               </div>
+              {type === 2 ? (
+                <div className="h-full w-full p-2">
+                  <div className=" w-full rounded bg-slate-50 p-2">
+                    <div className="mb-2 flex items-center gap-1">
+                      <div className=" h-4 w-2 bg-yellow-400" />
+                      <Text strong style={{ marginRight: '8px' }}>
+                        题目
+                      </Text>
+                      <Text type="secondary">附图</Text>
+                      <Checkbox
+                        checked={hasAttached}
+                        onChange={(e) => setHasAttached(e.target.checked)}
+                      />
+                    </div>
+                    <div className="flex flex-wrap gap-2">
+                      {titleImages.map((i) => (
+                        <Fragment key={i.slice(10)}>{imageBox(i)}</Fragment>
+                      ))}
+                      {uploadBox(() => setIsOpenTitle(true))}
+                    </div>
+                  </div>
+                  {hasAttached && (
+                    <div className=" my-2 w-full rounded bg-slate-50 p-2">
+                      <div className="mb-2 flex items-center gap-1">
+                        <div className=" h-4 w-2 bg-blue-400" />
+                        <Text strong>附图</Text>
+                      </div>
+                      <div className="flex flex-wrap gap-2">
+                        {attachedImages.map((i) => (
+                          <Fragment key={i.slice(10)}>{imageBox(i)}</Fragment>
+                        ))}
+                        {uploadBox(() => setIsOpenAttached(true))}
+                      </div>
+                    </div>
+                  )}
+
+                  <div className=" w-full rounded bg-slate-50 p-2">
+                    <div className="mb-2 flex items-center gap-1">
+                      <div className=" h-4 w-2 bg-green-400" />
+                      <Text strong>问题</Text>
+                    </div>
+                    <div className="flex flex-wrap gap-2">
+                      {questionImages.map((i) => (
+                        <Fragment key={i.slice(10)}>{imageBox(i)}</Fragment>
+                      ))}
+                      {uploadBox(() => setIsOpenQuestion(true))}
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <>
+                  <CameraFilled
+                    style={{
+                      fontSize: '16rem',
+                      color: 'var(--primary-color)',
+                      marginTop: '4rem',
+                    }}
+                    onClick={() => setIsOpen(true)}
+                  />
+                  <div
+                    className="flex flex-wrap gap-2 p-2"
+                    style={{ minHeight: '6rem', marginTop: '6rem' }}
+                  >
+                    <Divider />
+                    {images.map((i) => (
+                      <Fragment key={i.slice(10)}>{imageBox(i)}</Fragment>
+                    ))}
+                  </div>
+                </>
+              )}
             </div>
           )}
         </div>
@@ -160,6 +279,25 @@ function Home() {
         onChange={setImages}
         onFinally={() => setIsOpen(false)}
         isCompress
+        multiple={false}
+      />
+      <ImageUploadInput
+        isOpen={isOpenQuestion}
+        onChange={(files) => setQuestionImages((old) => [...old, ...files])}
+        onFinally={() => setIsOpenQuestion(false)}
+        multiple={false}
+      />
+      <ImageUploadInput
+        isOpen={isOpenTitle}
+        onChange={(files) => setTitleImages((old) => [...old, ...files])}
+        onFinally={() => setIsOpenTitle(false)}
+        multiple={false}
+      />
+      <ImageUploadInput
+        isOpen={isOpenAttached}
+        onChange={(files) => setAttachedImages((old) => [...old, ...files])}
+        onFinally={() => setIsOpenAttached(false)}
+        multiple={false}
       />
     </div>
   );
---
+++ src/types/home/map.ts
@@ -0,0 +1,26 @@
+export const subjectOptions = [
+  { label: '日语', value: 1 },
+  { label: '数学', value: 2 },
+  { label: '英语', value: 3 },
+  { label: '历史', value: 4 },
+  { label: '地理', value: 5 },
+];
+export const subjectMap = new Map<number, string>([
+  [1, '日语'],
+  [2, '数学'],
+  [3, '英语'],
+  [4, '历史'],
+  [5, '地理'],
+]);
+
+export const languageOptions = [
+  { label: '日语', value: 1 },
+  { label: '英语', value: 2 },
+  { label: '汉语', value: 3 },
+];
+
+export const languageMap = new Map<number, string>([
+  [1, '日语'],
+  [2, '英语'],
+  [3, '汉语'],
+]);
