abc2592aa4d00d18075c5c973e066482f8e737f8
nextdooroldwang <278188438@qq.com>
2023-11-13 14:40:51 +0900

    build

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/docssw.js', { scope: '/LetMeKnow' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/docssw.js', { scope: '/LetMeKnow/' })})}
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      base: '/LetMeKnow',
       buildBase: '/docs',
       registerType: 'autoUpdate',
       workbox: {
