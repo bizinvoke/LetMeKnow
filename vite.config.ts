a9ae93c9328705d5dbef0e5ccb7b1a70fcae8732
nextdooroldwang <278188438@qq.com>
2023-11-15 17:19:09 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      base: '/',
+      base: '/LetMeKnow/',
       registerType: 'autoUpdate',
       manifest: {
         name: 'LetMeKnow',
