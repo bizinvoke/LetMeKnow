19e7d8a30f7e8277216b578870fcec1649a89d65
nextdooroldwang <278188438@qq.com>
2023-11-15 22:17:49 +0900

    build

--- docs/assets/_layout-1489e59d.js
+++
@@ -1,1 +0,0 @@
-import{u as o,j as a,O as t,an as s}from"./index-3fb1ddbb.js";const e=["/sample"];function i(){const n=o().pathname;return e.includes(n)?a(t,{}):s("div",{className:"main-layout",children:["main layout",a(t,{})]})}function l(){return a(i,{})}export{l as default};
